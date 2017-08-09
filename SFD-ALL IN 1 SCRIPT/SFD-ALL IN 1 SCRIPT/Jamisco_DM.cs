f1b3925c83f2944917f1a44c4f14f98b961265e1
Jamisco <mufuhjames@gmail.com>
2017-08-09 13:36:24 -0400

    SIGNATURES!!!!

--- SFD-ALL IN 1 SCRIPT/SFD-ALL IN 1 SCRIPT/Jamisco_DM.cs
+++ SFD-ALL IN 1 SCRIPT/SFD-ALL IN 1 SCRIPT/Jamisco_DM.cs
@@ -11,57 +11,142 @@ namespace SFDConsoleApplication1
     {
         public Jamisco_DM() : base(null) { }
 
-        #region
-        Events.PlayerDeathCallback OnDeath = null;
-        static IPlayer[] initPlayers;
-        IProfile prfle = new IProfile();
-        static IPlayer plyer;
-        Vector2 spawnPos = new Vector2();
-        MapType map = MapType.Custom;
+        #region All_In_One
+//            _____                            _____                            _____                            _____                  
+//           /\    \                          /\    \                          /\    \                          /\    \                 
+//          /::\    \                        /::\    \                        /::\____\                        /::\    \                
+//          \:::\    \                      /::::\    \                      /::::|   |                        \:::\    \               
+//           \:::\    \                    /::::::\    \                    /:::::|   |                         \:::\    \              
+//            \:::\    \                  /:::/\:::\    \                  /::::::|   |                          \:::\    \             
+//             \:::\    \                /:::/__\:::\    \                /:::/|::|   |                           \:::\    \            
+//             /::::\    \              /::::\   \:::\    \              /:::/ |::|   |                           /::::\    \           
+//    _____   /::::::\    \            /::::::\   \:::\    \            /:::/  |::|___|______            ____    /::::::\    \          
+//   /\    \ /:::/\:::\    \          /:::/\:::\   \:::\    \          /:::/   |::::::::\    \          /\   \  /:::/\:::\    \         
+//  /::\    /:::/  \:::\____\        /:::/  \:::\   \:::\____\        /:::/    |:::::::::\____\        /::\   \/:::/  \:::\____\        
+//  \:::\  /:::/    \::/    /        \::/    \:::\  /:::/    /        \::/    / ~~~~~/:::/    /        \:::\  /:::/    \::/    /        
+//   \:::\/:::/    / \/____/          \/____/ \:::\/:::/    /          \/____/      /:::/    /          \:::\/:::/    / \/____/         
+//    \::::::/    /                            \::::::/    /                       /:::/    /            \::::::/    /                  
+//     \::::/    /                              \::::/    /                       /:::/    /              \::::/____/                   
+//      \::/    /                               /:::/    /                       /:::/    /                \:::\    \                   
+//       \/____/                               /:::/    /                       /:::/    /                  \:::\    \                  
+//                                            /:::/    /                       /:::/    /                    \:::\    \                 
+//                                           /:::/    /                       /:::/    /                      \:::\____\                
+//                                           \::/    /                        \::/    /                        \::/    /                
+//                                            \/____/                          \/____/                          \/____/                 
+//                                                                                                                                      
+//                            _____                            _____                           _______                                  
+//                           /\    \                          /\    \                         /::\    \                                 
+//                          /::\    \                        /::\    \                       /::::\    \                                
+//                         /::::\    \                      /::::\    \                     /::::::\    \                               
+//                        /::::::\    \                    /::::::\    \                   /::::::::\    \                              
+//                       /:::/\:::\    \                  /:::/\:::\    \                 /:::/~~\:::\    \                             
+//                      /:::/__\:::\    \                /:::/  \:::\    \               /:::/    \:::\    \                            
+//                      \:::\   \:::\    \              /:::/    \:::\    \             /:::/    / \:::\    \                           
+//                    ___\:::\   \:::\    \            /:::/    / \:::\    \           /:::/____/   \:::\____\                          
+//                   /\   \:::\   \:::\    \          /:::/    /   \:::\    \         |:::|    |     |:::|    |                         
+//                  /::\   \:::\   \:::\____\        /:::/____/     \:::\____\        |:::|____|     |:::|    |                         
+//                  \:::\   \:::\   \::/    /        \:::\    \      \::/    /         \:::\    \   /:::/    /                          
+//                   \:::\   \:::\   \/____/          \:::\    \      \/____/           \:::\    \ /:::/    /                           
+//                    \:::\   \:::\    \               \:::\    \                        \:::\    /:::/    /                            
+//                     \:::\   \:::\____\               \:::\    \                        \:::\__/:::/    /                             
+//                      \:::\  /:::/    /                \:::\    \                        \::::::::/    /                              
+//                       \:::\/:::/    /                  \:::\    \                        \::::::/    /                               
+//                        \::::::/    /                    \:::\    \                        \::::/    /                                
+//                         \::::/    /                      \:::\____\                        \::/____/                                 
+//                          \::/    /                        \::/    /                         ~~                                       
+//                           \/____/                          \/____/                                                                   
+//                                                                                                                                                                                                                                                                           
 
-        public void OnStartUp()
+        Events.PlayerDeathCallback m_playerDeathEvent = null;
+        static Events.UpdateCallback waitList_update = null;
+        static List<IObject> spawnPoints = new List<IObject>();
+        static Queue<IUser> Death_Q = new Queue<IUser>();
+        static Queue<IUser> Wait_Q = new Queue<IUser>();
+        static IObjectTimerTrigger timerTrigger = (IObjectTimerTrigger)Game.CreateObject("TimerTrigger");
+        static Random rnd = new Random();
+
+        public void OnStartup()
         {
-            initPlayers = new IPlayer[Game.GetPlayers().Length];
-
-            OnDeath = Events.PlayerDeathCallback.Start(OnPlayerDeath);
-            
+            waitList_update = Events.UpdateCallback.Start(DQ_WaitList, 5000);
+            m_playerDeathEvent = Events.PlayerDeathCallback.Start(OnPlayerDeath);
+            Game.SetMapType(MapType.Custom);
+            Game.DeathSequenceEnabled = false;
         }
 
-        public void Revive()
+        public void OnPlayerDeath(IPlayer player)
         {
-            foreach (IPlayer deadPlyr in Game.GetPlayers())
+            if (player != null && player.GetUser() != null)
             {
-                if (deadPlyr == null)
+                new Revive_Player(player.GetUser());
+            }
+            else if (player == null || player.GetUser() == null)
+            {
+                return;
+            }
+        }
+
+        public static IObject Spawn_Points()
+        {
+            foreach (IObject spawn in Game.GetObjects("Spawn_Point")) //will be edited for team based gameplay
+            {
+                spawnPoints.Add(spawn);
+            }
+            return spawnPoints[rnd.Next(spawnPoints.Count)];
+        }
+
+        public void Spawner(TriggerArgs args)
+        {
+            Revive_Player.Respawner();
+        }
+
+        public void DQ_WaitList(float elapsed)
+        {
+            if (Wait_Q.Count > 0)
+            {
+                Wait_Q.Dequeue();
+            }
+        }
+
+        public class Revive_Player
+        {
+            int wait_Timer = 1;
+            public Revive_Player(IUser user)
+            {
+                foreach (IUser usr in Wait_Q)
                 {
-                    prfle = deadPlyr.GetProfile();
+                    if (user == usr)
+                    {
+                        wait_Timer++;
+                    }
+                }
+                Death_Q.Enqueue(user);
+                CreateTimer(2000 * wait_Timer);
+            }
+
+            public static void Respawner()
+            {
+                if (Death_Q.Count > 0)
+                {
+                    IUser plr = Death_Q.Dequeue();
+                    Vector2 pos = Vector2.Zero;
+                    if (spawnPoints.Count > 0)
+                    {
+                        pos = Spawn_Points().GetWorldPosition();
+                    }
+                    IPlayer player = Game.CreatePlayer(pos);
+                    player.SetProfile(plr.GetProfile());
+                    player.SetUser(plr);
+                    Wait_Q.Enqueue(player.GetUser());
                 }
             }
 
-            foreach(IPlayer plyrs in Game.GetPlayers())
+            private void CreateTimer(int interval)
             {
-                if (plyrs.GetTeam() == plyer.GetTeam())
-                {
-                    spawnPos = plyrs.GetWorldPosition() + new Vector2(5, 5);
-                }
+                timerTrigger.SetIntervalTime(interval);
+                timerTrigger.SetRepeatCount(1);
+                timerTrigger.SetScriptMethod("Spawner");
+                timerTrigger.Trigger();
             }
-
-            IPlayer plyr = Game.CreatePlayer(spawnPos);
-            plyer.SetProfile(prfle);
-            plyr.SetUser(plyr.GetUser());
-            
-        }
-
-        public void OnPlayerDeath(IPlayer plyer)
-        {
-            ReviveTimer();
-        }
-
-        public void ReviveTimer()
-        {
-            IObjectTimerTrigger timer = (IObjectTimerTrigger)Game.CreateObject("TimerTrigger");
-            timer.SetIntervalTime(5000);
-            timer.SetScriptMethod("Revive");
-            timer.Trigger();
         }
         #endregion
     }
